a1b9c65ba520b105691b821753adbf3de831be49
berb <wojciech.kondratowicz@gmail.com>
2016-12-19 20:17:38 +0100

    test validators

--- src/test/java/logic/validators/ValidatorsTest.java
+++ src/test/java/logic/validators/ValidatorsTest.java
@@ -1,10 +1,12 @@
 package logic.validators;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.io.IOException;
 
+import org.junit.After;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -17,20 +19,44 @@ public class ValidatorsTest {
 	@Rule
     public TemporaryFolder folder = new TemporaryFolder();
 
+	File testFile;
+	
+	@After
+	public void afterEach() {
+		testFile.delete();
+	}
+
 	@Test(expected = FileDoesNotExistException.class)
 	public void testShouldThrowFileDoesNotExistExceptionWhenFileDoesNotExist() throws FileDoesNotExistException {
 		//given
 		String wrongPath = "wrongPath";
-		File testFile = new File(wrongPath);
+		testFile = new File(wrongPath);
 		//when
 		Validators.existFileValidator(testFile);
+	}
+	
+	@Test
+	public void testShouldReturnTrueWhenFileExist() throws FileDoesNotExistException {
+		//given
+		boolean expectedResult = true;
+		boolean actualResult;
+		String temporaryFile = "temporaryFile";
+		try {
+			testFile = folder.newFile(temporaryFile);
+		} catch (IOException e) {
+			fail("File Does Not Exist");
+		}
+		//when
+		actualResult = Validators.existFileValidator(testFile);
+		//then 
+		assertEquals(expectedResult,actualResult);
 	}
 	
 	@Test(expected = DirectoryDoesNotExistException.class)
 	public void testShouldThrowDirectoryDoesNotExistExceptionWhenIsDirectory() throws DirectoryDoesNotExistException {
 		//given
 		String wrongPath = "wrongPath";
-		File testFile = new File(wrongPath);
+		testFile = new File(wrongPath);
 		//when
 		Validators.existDirectoryValidator(testFile);
 	}
@@ -41,7 +67,6 @@ public class ValidatorsTest {
 		boolean expectedResult = true;
 		boolean actualResult;
 		String temporaryFolder = "temporaryFolder";
-		File testFile = null;
 		try {
 			testFile = folder.newFolder(temporaryFolder);
 		} catch (IOException e) {
